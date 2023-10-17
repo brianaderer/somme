d1d06ca38bbce2bae8c1245156327e0688c8cf21
Brian <brian@brianaderer.com>
2023-10-16 22:15:53 -0400

    .evn configured

--- SignIns/Google.js
+++ SignIns/Google.js
@@ -1,5 +1,1 @@
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
-GoogleSignin.configure({
-  webClientId: '',
-});
--- blocks/Login.js
+++ blocks/Login.js
@@ -3,6 +3,7 @@ import {StyledButton} from '../components/StyledButton';
 import React from 'react';
 import {StyledIcon} from '../components/StyledIcon';
 import {StyledButtonText} from '../components/StyledButtonText';
+import {KEY} from '@env';
 
 const Login = () => {
   return (
