982fc28135c7365c7fa6e29ca9dd4d1e10c282f0
Brian <brian@brianaderer.com>
2023-10-17 22:02:56 -0400

    making it through signin process but handshake failing

--- providers/Auth.js
+++ providers/Auth.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import {UserProvider} from '../contexts/UserContext';
-import firebase from '../modules/firebase';
 
 const AuthWrapper = ({children}) => {
   // Set an initializing state whilst Firebase connects
@@ -17,8 +16,7 @@ const AuthWrapper = ({children}) => {
   }
 
   useEffect(() => {
-    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    return auth().onAuthStateChanged(onAuthStateChanged); // unsubscribe on unmount
   }, []);
 
   return <UserProvider value={{user, initializing}}>{children}</UserProvider>;
