129352b4d3b20a91585046721a1da8cdc79e1d68
Brian <brian@brianaderer.com>
2023-11-05 15:57:25 -0500

    getting the wines out of the db in a map function

--- pages/Wines.js
+++ pages/Wines.js
@@ -1,7 +1,7 @@
 import {View, Text} from '@gluestack-ui/themed';
 import {StyledButton} from '../components/StyledButton';
 import {StyledButtonText} from '../components/StyledButtonText';
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import StylesContext from '../contexts/StylesContext';
 import UserContext from '../contexts/UserContext';
 import {addWine} from '../utilities/addWine';
@@ -10,15 +10,29 @@ import {bindActionCreators} from 'redux';
 import {changeMeta} from '../actions/meta';
 import {connect} from 'react-redux';
 import {getWines} from '../utilities/getWines';
+import firestore from '@react-native-firebase/firestore';
 
 const Wines = props => {
   const [retrievedWines, setRetrievedWines] = useState([]);
   const {location, requestLocation} = useLocation();
   const {navigation, meta, actions} = props;
   const {user, initializing} = useContext(UserContext);
-  const uid = user.providerData[0].uid;
-  getWines({uid}).then(data => {
-    console.log(data.docs);
+  const uid = user?.providerData[0]?.uid;
+  useEffect(() => {
+    const wine = firestore()
+      .collection('wines')
+      .orderBy('timestamp', 'desc')
+      .where('owner', '==', uid)
+      .onSnapshot(querySnapshot => {
+        // Map through the documents and return the data
+        const wines = querySnapshot.docs.map(doc => ({
+          id: doc.id, // don't forget to get the document ID if you need it
+          ...doc.data(), // spread operator to get all the fields in the object
+        }));
+        setRetrievedWines(wines); // Set the state with the new data
+      });
+    // Stop listening for updates when no longer required
+    return () => wine();
   });
   const addWineHandler = () => {
     addWine({uid, location, meta, actions}).then(
--- utilities/getWines.js
+++ utilities/getWines.js
@@ -3,6 +3,8 @@ import firestore from '@react-native-firebase/firestore';
 const winesCollection = firestore().collection('wines');
 export const getWines = async props => {
   const {uid} = props;
-  console.log(uid);
-  return await winesCollection.where('owner', '==', uid).get();
+  return await winesCollection
+    .orderBy('timestamp', 'desc')
+    .where('owner', '==', uid)
+    .get();
 };
